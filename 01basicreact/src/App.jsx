5ab19505b6ca421fd45e96b279fa3289ab3fc524
its.adityashah <adityakshah69@gmail.com>
2024-12-27 16:42:52 +0545

    setting up react

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Aditya } from './components/Aditya'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Aditya />
+    </>
+  )
+}
+
+export default App
